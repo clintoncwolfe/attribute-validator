012ee513a1f5cb6a17087620ecc36ade655b72b2
Clinton Wolfe <clintonc.wolfe@zenimaxonline.com>
2013-11-12 16:32:59 -0800

    add test for compile-time breaking run

--- test/unit/compile_spec.rb
+++ test/unit/compile_spec.rb
@@ -12,4 +12,18 @@ describe 'compilephase should work' do
     end
 
   end
+
+  context 'when running a broken compile run' do
+    context 'with the default fail action' do
+      output = `CAV_VAGRANTFILE_FIXTURE=compile-broken-default.Vagrantfile vagrant provision 2>&1 `
+      status = $?
+      it 'should mention the compile-time run in the info log' do
+        expect(output).to match /INFO: Running compile-time node attribute validations/
+      end
+      it 'should not successfully run' do
+        expect(status.exitstatus).to eql 1
+      end
+    end
+
+  end
 end
