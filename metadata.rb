841aeebf8d8919b020ebeab71e81ab9e5c486a47
Clinton Wolfe <clinton.wolfe@zenimaxonline.com>
2014-11-05 14:01:30 -0500

    Bump to v0.3.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'clinton@omniti.com'
 license          'BSD (3-clause)'
 description      'Enforces attribute validation rules using the chef-attribute-validator gem.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.2'
+version          '0.3.3'
 
 recipe 'install', 'Installs chef-attribute-validator gem to chef ruby gemstore'
 recipe 'compile-time-check', 'Enforces attribute validation rules at compile time'
