36cecb70a09b93701acc6ea6e96d3a12b25530a5
Clinton Wolfe <clinton.wolfe@zenimaxonline.com>
2014-11-05 14:01:16 -0500

    Change options to gem install to be a string to avoid hash error

--- recipes/install.rb
+++ recipes/install.rb
@@ -10,9 +10,9 @@ chef_gem "chef-attribute-validator" do
 
   # attribute-validator's only dep is chef ~> 11.6
   # Having the omnibus chef try to upgrade itself as a gem 
-  # is pretty much always a disaster; many native package, may 
+  # is pretty much always a disaster; many native packages, may 
   # not have a compiler, etc.
-  options :ignore_dependencies => true
+  options "--ignore-dependencies"
 end
 
 resources('chef_gem[chef-attribute-validator]').run_action(:install)
