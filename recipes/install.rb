d52a5c2b902c27490ef78620c6d30e68d7185ece
Clinton Wolfe <clinton.wolfe@zenimaxonline.com>
2014-11-10 12:09:53 -0500

    Hrm, remove forced-compile-time execution of chef_gem since that's supposed to happen automatically, and forcing it broke Gem.clear_paths, leading to being unable to find the gem.

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,15 +6,10 @@
 # 
 
 chef_gem "chef-attribute-validator" do
-  action :nothing
-
   # attribute-validator's only dep is chef ~> 11.6
   # Having the omnibus chef try to upgrade itself as a gem 
   # is pretty much always a disaster; many native packages, may 
   # not have a compiler, etc.
   options "--ignore-dependencies"
 end
-
-resources('chef_gem[chef-attribute-validator]').run_action(:install)
-
 require 'chef-attribute-validator'
