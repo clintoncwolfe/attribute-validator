e9ebddbf34d8c64243ea585029b04fc8eb7356f3
Clinton Wolfe <clinton@omniti.com>
2015-10-13 21:40:18 -0400

    Allow installation of gems from local cookbook_files dir

--- recipes/install.rb
+++ recipes/install.rb
@@ -5,11 +5,24 @@
 # Copyright (C) 2013 Clinton Wolfe
 # 
 
-chef_gem "chef-attribute-validator" do
-  # attribute-validator's only dep is chef ~> 11.6
-  # Having the omnibus chef try to upgrade itself as a gem 
-  # is pretty much always a disaster; many native packages, may 
-  # not have a compiler, etc.
-  options "--ignore-dependencies"
+# Undocumented: for development, you may set node['attribute-validator']['local-install'] to a version string
+# and we will install the matching gem from the local files/ directory of the cookbook
+if node['attribute-validator']['local-install'] then
+  r = cookbook_file '/tmp/chef-attribute-validator-' + node['attribute-validator']['local-install'] + '.gem'
+  r.run_action(:create)
+  chef_gem "chef-attribute-validator" do
+    source '/tmp/chef-attribute-validator-' + node['attribute-validator']['local-install'] + '.gem'
+    options "--ignore-dependencies"
+  end
+else
+  
+  chef_gem "chef-attribute-validator" do
+    # attribute-validator's only dep is chef ~> 11.6
+    # Having the omnibus chef try to upgrade itself as a gem 
+    # is pretty much always a disaster; many native packages, may 
+    # not have a compiler, etc.
+    options "--ignore-dependencies"
+  end
 end
+
 require 'chef-attribute-validator'
